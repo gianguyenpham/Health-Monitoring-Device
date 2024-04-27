20a12c38fe0061cb1d542aef4d55493f4d43f680
gianguyenpham <141945018+gianguyenpham@users.noreply.github.com>
2024-04-27 22:08:26 +0700

    Add files via upload

---
+++ REDONE_no_ECG_serialmonitor_UNTESTED.ino
@@ -0,0 +1,196 @@
+#include <Wire.h>
+#include "DFRobot_AHT20.h"
+#include "DFRobot_ENS160.h"
+#include "Protocentral_MAX30205.h"
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+
+DFRobot_AHT20 aht20;
+DFRobot_ENS160_I2C ens160(&Wire, 0x53);
+MAX30205 max30205;
+MAX30105 particleSensor;
+
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+byte pulseLED = 11; //Must be on PWM pin
+byte readLED = 13; //Blinks with each data read
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+
+  // Initialize AHT20 sensor
+  uint8_t status;
+  while ((status = aht20.begin()) != 0) {
+    Serial.print("AHT20 sensor initialization failed. Error status: ");
+    Serial.println(status);
+    delay(1000);
+  }
+
+  // Initialize ENS160 sensor
+  while (NO_ERR != ens160.begin()) {
+    Serial.println("Communication with ENS160 sensor failed, please check connection");
+    delay(3000);
+  }
+  Serial.println("ENS160 Begin ok!");
+
+  ens160.setPWRMode(ENS160_STANDARD_MODE);
+  ens160.setTempAndHum(25.0, 50.0);
+
+  // Initialize MAX30205 sensor
+  while (!max30205.scanAvailableSensors()) {
+    Serial.println("Couldn't find the MAX30205 temperature sensor, please connect the sensor.");
+    delay(30000);
+  }
+  max30205.begin();
+
+  // Initialize MAX30105 sensor
+  
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
+    while (1);
+  }
+
+  /*Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0) ; //wait until user presses a key
+  Serial.read();*/
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+
+void loop() {
+  // AHT20
+  if (aht20.startMeasurementReady(/* crcEn = */true)) {
+    Serial.print("AHT20 - Temperature: ");
+    Serial.print(aht20.getTemperature_C());
+    Serial.println(" 'C");
+
+    Serial.print("AHT20 - Humidity: ");
+    Serial.print(aht20.getHumidity_RH());
+    Serial.println(" %RH");
+  }
+
+  // ENS160
+  Serial.print("ENS160 - Air quality index: ");
+  Serial.println(ens160.getAQI());
+
+  Serial.print("ENS160 - Concentration of total volatile organic compounds: ");
+  Serial.print(ens160.getTVOC());
+  Serial.println(" ppb");
+
+  Serial.print("ENS160 - Carbon dioxide equivalent concentration: ");
+  Serial.print(ens160.getECO2());
+  Serial.println(" ppm");
+
+  // MAX30205
+  float temp = max30205.getTemperature();
+  Serial.print("MAX30205 - Body temperature: ");
+  Serial.print(temp, 2);
+  Serial.println(" 'C");
+
+  // MAX30105
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    /*Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);*/
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      /*Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);*/
+
+      //Serial.print(F(", HR="));
+      //Serial.println(heartRate, DEC);
+
+      //Serial.print(F(", HRvalid="));
+      //Serial.print(validHeartRate, DEC);
+
+      //Serial.print(F(", SPO2="));
+      //Serial.println(spo2, DEC);
+
+      //Serial.print(F(", SPO2Valid="));
+      //Serial.println(validSPO2, DEC);
+
+      Serial.print("MAX30105 - Heart rate: ");
+      Serial.print(heartRate);
+      Serial.println(" BPM");
+
+      Serial.print("MAX30105 - SPO2: ");
+      Serial.print(spo2);
+      Serial.println(" %");
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  Serial.println();
+  delay(1);
+}
+}
